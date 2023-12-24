d23abb8a7b79901f1978c153bb47ea38b1585c5f
AmritaTiwary1 <amritatiwary112@gmail.com>
2023-12-24 13:38:30 +0530

    first commit

--- hello/urls.py
+++ hello/urls.py
@@ -19,8 +19,8 @@ from django.urls import path , include
 
 #for changing text of admin page = https://books.agiliq.com/projects/django-admin-cookbook/en/latest/change_text.html#:~:text=How%20to%20change%20%E2%80%98Django%20administration%E2%80%99%20text%3F%20%C2%B6%201,and%20is%20set%20to%20%E2%80%9CDjango%20site%20admin%E2%80%9D%20
 #i went to this link and copy the code and make some changes like "amrita ice cream"
-admin.site.site_header = "Amrita Ice Cream Admin"
-admin.site.site_title = "ICE CREAM Admin Portal"
+admin.site.site_header = "Amrita ShopJoy Admin"
+admin.site.site_title = "ShopJoy Admin Portal"
 admin.site.index_title = "Welcome to MY SHOP "
 
 
